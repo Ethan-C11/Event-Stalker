3bab0dcab519adf53be2753c35305683bfcaa37e
Ethan <ethan.collignon.1102@gmail.com>
2026-01-21 22:59:00 +0100

    feat: wip sqlite

---
+++ db/schema.js
@@ -0,0 +1,9 @@
+const { sqliteTable, text, integer } = require('drizzle-orm/sqlite-core');
+
+export const eventStalkerWebhooks = sqliteTable('eventStalkerWebhooks', {
+    id: text('id').primaryKey(),
+    guildId : text('guildId'),
+    channelId : text('channelId'),
+    organizationSlug : text('organizationSlug'),
+    webhookUrl : text('webhookUrl'),
+})
