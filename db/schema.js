efae3a64fdb26e8839b89dc0bcccb2085c53e422
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-01-23 14:50:49 +0100

    feat: removed unused column

--- db/schema.js
+++ db/schema.js
@@ -2,10 +2,7 @@ const { sqliteTable, text, int } = require('drizzle-orm/sqlite-core');
 
 const stalker_webhooks_table = sqliteTable('stalker_webhooks_table', {
     id: int().primaryKey({ autoIncrement: true }),
-    guildId : text().notNull(),
-    channelId : text().notNull(),
     organizationSlug : text().notNull(),
-    webhookId : text().notNull(),
     webhookUrl : text().notNull(),
 })
 
